6517d9e8d3ecf24da5589332e47570c33f7ead0c
Pedro Medeiros <pmfa2805@gmail.com>
2025-01-30 10:52:55 -0300

    Config. express server

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,42 +1,21 @@
 import { PrismaClient } from '@prisma/client'
+import { Request, Response } from 'express'
 
 const prisma = new PrismaClient()
 
-async function main() {
-  // ... you will write your Prisma Client queries here
-//   await prisma.user.create({
-//     data: {
-//       name: 'Alice',
-//       email: 'alice@prisma.io',
-//       posts: {
-//         create: { title: 'Hello World' },
-//       },
-//       profile: {
-//         create: { bio: 'I like turtles' },
-//       },
-//     },
-//   })
-const post = await prisma.post.update({
-    where: { id: 1 },
-    data: { published: true },
-  })
-  console.log(post)
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-  console.dir(allUsers, { depth: null })
+export async function allUsers(req: Request, res: Response) {
+  const users = await prisma.user.findMany(
+
+  )
+  res.status(200).send(users)
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
+export async function findUser(req: Request, res: Response) {
+  const users = await prisma.user.findUnique({
+    where: {
+      id: parseInt(req.params['id'])
+    }
   })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+  res.status(200).send(users)
+}
---
+++ Backend/src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+import { allUsers, findUser } from '.';
+
+const app = express();
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Teste' })
+})
+
+route.get('/hello-world/', (req: Request, res: Response) => {
+    res.json({ message: 'hello world with Typescript' })
+})
+
+app.use(route)
+
+
+route.get('/allusers/', allUsers)
+route.get('/allusers/:id', findUser)
+
+
+
+
+app.listen(3333, () => 'server running on port 3333')
