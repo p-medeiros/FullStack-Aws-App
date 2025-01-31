e7f98c6bb28348e4ab681cca66db93da6d5e600c
Pedro Medeiros <pmfa2805@gmail.com>
2025-01-31 11:36:43 -0300

    update Routes

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,15 +5,15 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import App from './App';
-import Dashboard from './Dashboard';
+import Dashboard from './pages/Dashboard';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <ThemeProvider theme={theme}> */}
     <StyledEngineProvider injectFirst>
       {/* <CssBaseline /> */}
-      {/* <App /> */}
-      <Dashboard />
+      <App />
+      {/* <Dashboard /> */}
     </StyledEngineProvider>
     {/* </ThemeProvider> */}
   </React.StrictMode>,
--- Frontend/src/pages/Dashboard/index.tsx
+++ Frontend/src/pages/Dashboard/index.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
-import type {} from '@mui/x-date-pickers/themeAugmentation';
-import type {} from '@mui/x-charts/themeAugmentation';
-import type {} from '@mui/x-data-grid-pro/themeAugmentation';
-import type {} from '@mui/x-tree-view/themeAugmentation';
+import type { } from '@mui/x-date-pickers/themeAugmentation';
+import type { } from '@mui/x-charts/themeAugmentation';
+import type { } from '@mui/x-data-grid-pro/themeAugmentation';
+import type { } from '@mui/x-tree-view/themeAugmentation';
 import { alpha } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import AppNavbar from './components/AppNavbar';
-import Header from './components/Header';
-import MainGrid from './components/MainGrid';
-import SideMenu from './components/SideMenu';
-import AppTheme from './theme/AppTheme';
+import AppNavbar from '../../components/AppNavbar';
+import Header from '../../components/Header';
+import MainGrid from '../../components/MainGrid';
+import SideMenu from '../../components/SideMenu';
+import AppTheme from '../../theme/AppTheme';
 import {
   chartsCustomizations,
   dataGridCustomizations,
   datePickersCustomizations,
   treeViewCustomizations,
-} from './theme/customizations';
+} from '../../theme/customizations';
 
 const xThemeComponents = {
   ...chartsCustomizations,
