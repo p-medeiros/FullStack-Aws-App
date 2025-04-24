79c2adf955ad607b40b0ae3c8f1a21ba5c9fd04c
Pedro Medeiros <pmfa2805@gmail.com>
2025-04-24 11:03:02 -0300

    Fix

--- Frontend/build.sh
+++ Frontend/build.sh
@@ -1,5 +1,4 @@
-ECR_REGISTRY="934007006033.dkr.ecr.us-east-2.amazonaws.com"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t fullstack_aws_app .
-docker tag fullstack_aws_app:latest $ECR_REGISTRY/fullstack_aws_app:latest
-docker push $ECR_REGISTRY/fullstack_aws_app:latest
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 934007006033.dkr.ecr.us-east-2.amazonaws.com
+docker build -t fullstack_aws_app_Front .
+docker tag fullstack_aws_app:latest 934007006033.dkr.ecr.us-east-2.amazonaws.com/fullstack_aws_app_Front:latest
+docker push 934007006033.dkr.ecr.us-east-2.amazonaws.com/fullstack_aws_app_Front:latest
